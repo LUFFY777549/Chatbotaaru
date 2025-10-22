1f86c9ed281fce217ef1d635d4bfc5af9ac1c867
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-22 23:40:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,41 +1,38 @@
 from os import getenv
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-API_ID = int(getenv("API_ID", None))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", None))
-LOGGER_ID = int(getenv("LOGGER_ID", -1002036001760))
-MONGO_URL = getenv("MONGO_URL", None)
-AUTH_CHANNEL = int(getenv("AUTH_CHANNEL", None))
-FSUB = getenv("FSUB", False)
-OWNER_ID = int(getenv("OWNER_ID", None))
-WEB_APP = getenv("WEB_APP", False) 
-
+API_ID = int(getenv("API_ID", 21218274))
+API_HASH = getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
+BOT_TOKEN = getenv("BOT_TOKEN", "7442709602:AAE0Z65zpbiPnyMRAmxZISKS0m_V3soJd7w")
+OWNER_ID = int(getenv("OWNER_ID", 7019600964))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002345678901))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://luffybot:12345@cluster0.mongodb.net/?retryWrites=true&w=majority")
+AUTH_CHANNEL = int(getenv("AUTH_CHANNEL", -1002955024896))
+FSUB = int(getenv("FSUB", -1002955024896))
+WEB_APP = getenv("WEB_APP", "False").lower() == "true"
 
 STICKER = [
-"CAACAgUAAx0CeVrr4AABA2WaaCQoVC8FjSD8VuVTOemf8iMsuB4AAsUXAALfySFVZ8WvmJF9y7QeBA",
-"CAACAgUAAx0CeVrr4AABA2WbaCQoVFGQ8bMbYbjdAa_hwMX5Ey0AArQVAALa2yBVNyiv0PjLavkeBA",
-"CAACAgUAAx0CeVrr4AABA2WZaCQoTzXsmocAAV4ei8U4CHEaiCOkAAJtFQACDZkgVdlv6ZpBRa5OHgQ",
-"CAACAgUAAx0CeVrr4AABA2WYaCQoTLix5yKaiPC6VIAUGSQ50_0AAoIWAAL_vCFV9UCakfSiUnQeBA",
-"CAACAgUAAx0CeVrr4AABA2WXaCQoSN2AR6W-2usiKLpMy2WSdk0AAqcaAAKXHiFVeW8TyEoCbEMeBA",
-"CAACAgUAAx0CeVrr4AABA2WVaCQoOY7lsoxoeywegfiOQxVc74MAAs0TAALlgyFVlZxoUNHWXFAeBA",
-"CAACAgUAAx0CeVrr4AABA2WUaCQoNE9U5q9W1pgSXj9bpRTnoXgAAkYXAAI68ylV_qo6hecGh40eBA",
-"CAACAgUAAx0CeVrr4AABA2WDaCQnvUjQgOKVm5deqrv4_Jmkd14AAgoUAAJ3uCBVq_-M6rUuoi8eBA",
-"CAACAgUAAx0CeVrr4AABA2WEaCQnwECiKsxllioAAXTiYi5RPD_OAAKbFwACXCMpVcc_pIRGEhGNHgQ",
-"CAACAgUAAx0CeVrr4AABA2WFaCQnx1KB260wSbHWf4iOv9X3aVIAAm8VAAK0CyFV9AlM_s8podUeBA",
-"CAACAgUAAx0CeVrr4AABA2WHaCQn2-MhtiM4QpdcLNjOQ0imRkQAAukUAAIgkCFV5eb3HtvsISUeBA",
-"CAACAgUAAx0CeVrr4AABA2WIaCQn5GYTy5yUj-1fyHsYqI9bccQAAgMaAAI13CFVbtM4jrz2LkoeBA",
+    "CAACAgUAAx0CeVrr4AABA2WaaCQoVC8FjSD8VuVTOemf8iMsuB4AAsUXAALfySFVZ8WvmJF9y7QeBA",
+    "CAACAgUAAx0CeVrr4AABA2WbaCQoVFGQ8bMbYbjdAa_hwMX5Ey0AArQVAALa2yBVNyiv0PjLavkeBA",
+    "CAACAgUAAx0CeVrr4AABA2WZaCQoTzXsmocAAV4ei8U4CHEaiCOkAAJtFQACDZkgVdlv6ZpBRa5OHgQ",
+    "CAACAgUAAx0CeVrr4AABA2WYaCQoTLix5yKaiPC6VIAUGSQ50_0AAoIWAAL_vCFV9UCakfSiUnQeBA",
+    "CAACAgUAAx0CeVrr4AABA2WXaCQoSN2AR6W-2usiKLpMy2WSdk0AAqcaAAKXHiFVeW8TyEoCbEMeBA",
+    "CAACAgUAAx0CeVrr4AABA2WVaCQoOY7lsoxoeywegfiOQxVc74MAAs0TAALlgyFVlZxoUNHWXFAeBA",
+    "CAACAgUAAx0CeVrr4AABA2WUaCQoNE9U5q9W1pgSXj9bpRTnoXgAAkYXAAI68ylV_qo6hecGh40eBA",
+    "CAACAgUAAx0CeVrr4AABA2WDaCQnvUjQgOKVm5deqrv4_Jmkd14AAgoUAAJ3uCBVq_-M6rUuoi8eBA",
+    "CAACAgUAAx0CeVrr4AABA2WEaCQnwECiKsxllioAAXTiYi5RPD_OAAKbFwACXCMpVcc_pIRGEhGNHgQ",
+    "CAACAgUAAx0CeVrr4AABA2WFaCQnx1KB260wSbHWf4iOv9X3aVIAAm8VAAK0CyFV9AlM_s8podUeBA",
+    "CAACAgUAAx0CeVrr4AABA2WHaCQn2-MhtiM4QpdcLNjOQ0imRkQAAukUAAIgkCFV5eb3HtvsISUeBA",
+    "CAACAgUAAx0CeVrr4AABA2WIaCQn5GYTy5yUj-1fyHsYqI9bccQAAgMaAAI13CFVbtM4jrz2LkoeBA",
 ]
 
-
 IMG = [
-"https://files.catbox.moe/rz42be.jpg",
-"https://files.catbox.moe/41e7z2.jpg",
-"https://files.catbox.moe/q4o6l9.jpg",
-"https://files.catbox.moe/q4o6l9.jpg",
-"https://files.catbox.moe/0mpi0w.jpg",
-"https://files.catbox.moe/jnmurs.jpg",
+    "https://files.catbox.moe/rz42be.jpg",
+    "https://files.catbox.moe/41e7z2.jpg",
+    "https://files.catbox.moe/q4o6l9.jpg",
+    "https://files.catbox.moe/q4o6l9.jpg",
+    "https://files.catbox.moe/0mpi0w.jpg",
+    "https://files.catbox.moe/jnmurs.jpg",
 ]
